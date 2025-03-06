1b1be081cdded57545eec95c0404b6e304d01c98
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-03-06 15:43:00 +0300

    solved class owner

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -28,5 +28,18 @@ class Pet:
             raise Exception("Object is not of type Owner")
         self._owner = owner
 
+class Owner:
+    def __init__(self, name):
+        self.name = name
 
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Input object is not of type Pet")
+        pet.owner = self            
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
 
