7a8bfa24de94dfa2fd64f24301b6ca8edd755002
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-03-06 15:41:11 +0300

    finished pet class

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
 
-class Owner:
-    pass
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type not in self.PET_TYPES:
+            raise Exception('Not a valid pet type.')
+        self._pet_type = pet_type
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        if not (isinstance(owner, Owner) or not owner):
+            raise Exception("Object is not of type Owner")
+        self._owner = owner
+
+
+
